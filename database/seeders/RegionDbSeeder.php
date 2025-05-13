1d117c9991da74190cd88fbd55372eadd4606081
Vivek kumar <vivekrsq@gmail.com>
2025-05-13 10:21:00 +0530

    Change India code

--- database/seeders/RegionDbSeeder.php
+++ database/seeders/RegionDbSeeder.php
@@ -15,7 +15,7 @@ class RegionDbSeeder extends Seeder
     {
         RegionDb::insert([
             [
-                'region_name' => 'IN',
+                'region_name' => 'IND',
                 'db_host' => '127.0.0.1',
                 'db_port' => 3306,
                 'db_database' => 'simu_lara',
