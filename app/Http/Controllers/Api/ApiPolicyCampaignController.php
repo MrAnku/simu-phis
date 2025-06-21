d9f834e57ca21ff0347843db1a2e0544574a8894
Vivek Kumar <vivekrsq@gmail.com>
2025-06-21 15:48:15 +0530

    live the policy campaign when the schedule time is less than current time

--- app/Http/Controllers/Api/ApiPolicyCampaignController.php
+++ app/Http/Controllers/Api/ApiPolicyCampaignController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Users;
+use App\Models\UsersGroup;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Models\PolicyCampaign;
@@ -20,15 +22,52 @@ class ApiPolicyCampaignController extends Controller
                 'policy' => 'required|exists:policies,id',
                 'scheduled_at' => 'required|string',
             ]);
+            // check if scheduled_at is less than current time then the status will be running else pending
+            $scheduledAt = strtotime($request->scheduled_at);
+            $currentTime = time();
+            $status = $scheduledAt < $currentTime ? 'running' : 'pending';
+
+
 
             $campaign = PolicyCampaign::create([
                 'campaign_name' => $request->campaign_name,
                 'campaign_id' => Str::random(6),
                 'users_group' => $request->users_group,
                 'policy' => $request->policy,
+                'status' => $status,
                 'scheduled_at' => $request->scheduled_at,
                 'company_id' => Auth::user()->company_id,
             ]);
+            //send the employees to live table if status is running
+            if ($status === 'running') {
+                // Retrieve the users in the specified group
+                $userIdsJson = UsersGroup::where('group_id', $campaign->users_group)
+                    ->where('company_id', Auth::user()->company_id)
+                    ->value('users');
+
+                $userIds = json_decode($userIdsJson, true);
+                $users = Users::whereIn('id', $userIds)->get();
+
+                // $users = Users::where('group_id', $campaign->users_group)->get();
+
+                // Check if users exist in the group
+                if ($users->isEmpty()) {
+                    return response()->json(['success' => false, 'message' => __('No employees available in this group')], 404);
+                }
+
+                // Iterate through the users and create CampaignLive entries
+                foreach ($users as $user) {
+                    $campaign->campLive()->create([
+                        'campaign_id' => $campaign->campaign_id,
+                        'campaign_name' => $campaign->campaign_name,
+                        'user_name' => $user->user_name,
+                        'user_email' => $user->user_email,
+                        'sent' => 0,
+                        'policy' => $campaign->policy,
+                        'company_id' => Auth::user()->company_id,
+                    ]);
+                }
+            }
             // log_action("Policy campaign created for company: " . Auth::user()->company_id);
 
             return response()->json([
