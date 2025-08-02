54d4d50d73af329bbb3394114821b08d0323af55
SANA FARHEEN <sanafarheen29837373@gmail.com>
2025-08-02 23:22:20 +0530

    some fix

--- app/Http/Controllers/LearnApi/ApiLearnController.php
+++ app/Http/Controllers/LearnApi/ApiLearnController.php
@@ -981,7 +981,7 @@ class ApiLearnController extends Controller
                     'total_assigned_scorm_mod' => count($assignedScormModules),
                     'avg_scorm_score' => count($assignedScormModules) > 0 ? round(array_sum(array_column($assignedScormModules, 'score')) / count($assignedScormModules)) : 0,
                     'avg_scorm_grade' => $avgScormGrade,
-                    'total_trainings' => count($assignedTrainingModules) + count($assignedScormModules),
+                    'total_trainings' => TrainingAssignedUser::where('user_email', $request->email)->count() + ScormAssignedUser::where('user_email', $request->email)->count(),
                     'total_passed_trainings' => TrainingAssignedUser::where('user_email', $request->email)
                         ->where('completed', 1)->count() + ScormAssignedUser::where('user_email', $request->email)
                         ->where('completed', 1)->count(),
