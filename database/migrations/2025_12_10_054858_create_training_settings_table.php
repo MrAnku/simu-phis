12f02f2c82368c40a5ad2265a98362e604a381ae
kavitasparrowhost26 <kavita@sparrowhost.in>
2025-12-12 15:31:17 +0530

    recored fetch using model

--- database/migrations/2025_12_10_054858_create_training_settings_table.php
+++ database/migrations/2025_12_10_054858_create_training_settings_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Company;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,30 +19,8 @@ class CreateTrainingSettingsTable extends Migration
         });
 
         // Select all fields from company table
-        $records = DB::table('company')
-            ->select(
-                'email',
-                'full_name',
-                'company_name',
-                'company_id',
-                'partner_id',
-                'employees',
-                'storage_region',
-                'approved',
-                'service_status',
-                'password',
-                'pass_create_token',
-                'account_type',
-                'mssp_id',
-                'created_at',
-                'approve_date',
-                'usedemployees',
-                'role',
-                'enabled_feature',
-                'ip_whitelist'
-            )->get();
-
-        if ($records->isNotEmpty()) {
+       $records = Company::all();
+       if ($records->isNotEmpty()) {
             foreach ($records as $record) {
                 DB::table('training_settings')->insert([
                     'company_id'       => $record->company_id,
